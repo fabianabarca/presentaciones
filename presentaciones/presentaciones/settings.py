b616c83c7c730e67dd35f28dd0b9a5420ef8759d
Fabián Abarca <ensinergia@gmail.com>
2023-04-27 17:40:37 -0600

    Crear nueva SECRET_KEY y ocultarla en archivo .env

--- presentaciones/presentaciones/settings.py
+++ presentaciones/presentaciones/settings.py
@@ -10,20 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from pathlib import Path
+from decouple import config
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&86eis()23dwam5=e!g4_@fdg^g$4*kd&o@%6(7p-b**&-@m9-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
