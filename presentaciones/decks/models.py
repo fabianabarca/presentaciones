5095df24a414d11c2f20afda57882d85bef8ebd3
WilmerAraya <wilmeraraya21@gmail.com>
2023-10-18 04:19:44 -0600

    Creacion de modelos para preguntas

--- presentaciones/decks/models.py
+++ presentaciones/decks/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Modelos
+class Topic(models.Model):
+    topic_id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.topic_id}. {self.name}"
+    
 class Question(models.Model):
     question_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    equation = models.CharField(max_length=255, null=True, blank=True)
     image = models.ImageField(upload_to='question_images/', null=True, blank=True)
-    data = models.JSONField(null=True, blank=True)
-    type = models.CharField(max_length=255)
-    set = models.CharField(max_length=255)
-    topic = models.CharField(max_length=255)
+    data = models.FileField(null=True)
+    form_type = models.CharField(max_length=255)
+    question_set = models.CharField(max_length=255) #?
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE) # Foreing Key
 
     def __str__(self):
         return self.title
@@ -19,7 +24,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     choice_id = models.AutoField(primary_key=True)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     is_correct = models.BooleanField(default=False)
 
@@ -29,9 +34,10 @@ class Choice(models.Model):
 
 class Answer(models.Model):
     answer_id = models.AutoField(primary_key=True)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    user_id = models.IntegerField()  # Cambia esto según tu implementación de usuarios
-    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user_id = models.IntegerField()
+    choice_id = models.ForeignKey(Choice, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Respuesta a la pregunta {self.question_id} por el usuario {self.user_id}"
+
