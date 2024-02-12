74ee741d8f7cb5a9b0f0b29748cc935d1bd704f3
yasty-sanchez <yasty2012@gmail.com>
2024-02-11 22:06:49 -0600

    Agrego fixture de Snippets.

--- presentaciones/demo/views.py
+++ presentaciones/demo/views.py
@@ -9,66 +9,17 @@ def demo(request):
 
 
 def programming(request):
-    snippet1234 = Snippet.objects.get(id="1234")
-    snippet5678 = Snippet.objects.get(id="5678")
+    snippet1 = Snippet.objects.get(id="snippet1")
+    snippet2 = Snippet.objects.get(id="snippet2")
+    snippet3 = Snippet.objects.get(id="snippet3")
+    snippet4 = Snippet.objects.get(id="snippet4")
+    snippet5 = Snippet.objects.get(id="snippet5")
     context = {
-        "snippet1234": snippet1234,
-        "snippet5678": snippet5678,
-        "repl1": """
-        a = 5
-        b = 6
-        c = 7
-        s = (a + b + c) / 2
-        area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-
-        print('El área del triángulo es %0.2f.' %area)
-        print()
-        """,
-        "repl2": """
-        import numpy as np
-
-        array = np.array([
-            [3, 7, 1],
-            [10, 3, 2],
-            [5, 6, 7]
-        ])
-
-        print(array)
-        print()
-        print(np.sort(array, axis=None))
-        print(np.sort(array, axis=1))
-        print(np.sort(array, axis=0))
-        """,
-        "repl3": """
-        from scipy import linalg
-        import numpy as np
-
-        arr = np.array([[5,4],[6,3]])
-        eg_val, eg_vect = linalg.eig(arr)
-        print(eg_val)
-        print(eg_vect)
-        print()
-        """,
-        "repl4": """
-        import matplotlib.pyplot as plt
-        import numpy as np
-
-        x = np.random.randn(1000)
-        y = np.random.randn(1000)
-        fig, ax = plt.subplots()
-        ax.scatter(x, y)
-        fig
-        """,
-        "repl5": """
-        import pandas as pd
-
-        df = pd.DataFrame({
-            'num_legs': [2, 4, 8, 0],
-            'num_wings': [2, 0, 0, 0],
-            'num_specimen_seen': [10, 2, 1, 8]},
-            index=['falcon', 'dog', 'spider', 'fish'])
-        df
-        """
+        "snippet1": snippet1,
+        "snippet2": snippet2,
+        "snippet3": snippet3,
+        "snippet4": snippet4,
+        "snippet5": snippet5
     }
     return render(request, "programming.html", context)
 
