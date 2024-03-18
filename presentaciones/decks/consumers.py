401256699c5fecf28962291134588d002b2e8df6
WilmerAraya <wilmeraraya21@gmail.com>
2024-03-17 19:12:49 -0600

    Agrego funcion en consumer que guarda en la base de datos las respuestas

--- presentaciones/decks/consumers.py
+++ presentaciones/decks/consumers.py
@@ -1,6 +1,9 @@
 import json
 
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from decks.models import Answer, Session
 
 
 class DeckSliderConsumer(AsyncWebsocketConsumer):
@@ -20,14 +23,22 @@ class DeckSliderConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        indexh = text_data_json["indexh"]
-        indexv = text_data_json["indexv"]
 
-        # Send message to demo group
-        await self.channel_layer.group_send(
-            self.deck_group_name,
-            {"type": "slide_changed", "message": {"indexh": indexh, "indexv": indexv}},
-        )
+        if "answer" in text_data_json:
+            await self.save_answer(text_data_json)
+
+        else:
+            indexh = text_data_json["indexh"]
+            indexv = text_data_json["indexv"]
+
+            # Send message to demo group
+            await self.channel_layer.group_send(
+                self.deck_group_name,
+                {
+                    "type": "slide_changed",
+                    "message": {"indexh": indexh, "indexv": indexv},
+                },
+            )
 
     # Receive message from room group
     async def slide_changed(self, event):
@@ -35,3 +46,21 @@ class DeckSliderConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
+
+    @sync_to_async
+    def save_answer(self, answer_data):
+        print(answer_data)
+        answer_text = answer_data["answer"]
+        session_id = answer_data["session_id"]
+        question_id = answer_data["question_id"]
+        user_id = answer_data["user_id"]
+
+        session = Session.objects.get(session_id=int(session_id))
+        answer = Answer.objects.create(
+            question_id=int(question_id),
+            user_id=user_id,
+            answer_text=answer_text,
+            session_id=session,
+        )
+
+        answer.save()
