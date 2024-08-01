bdeaa45c96309337bf7ec1db8ad7883b19788621
yasty-sanchez <yasty2012@gmail.com>
2024-07-31 19:49:02 -0600

    Se implementa un "repl" con PyScript y Ace.js.

--- demo/views.py
+++ demo/views.py
@@ -16,9 +16,20 @@ def project(request):
     return render(request, "project.html", context)
 
 
-
 def programming(request):
-    return render(request, "programming.html")
+    context = {
+        "example1": """import numpy as np
+
+display(np.sqrt(4))""",
+        "example2": """import numpy as np
+import pandas as pd
+from pyscript import display
+
+df = pd.DataFrame(np.random.randn(10, 4), columns=list('ABCD')).cumsum()
+display(df)""",
+    }
+
+    return render(request, "programming.html", context)
 
 
 def visualizations(request):
